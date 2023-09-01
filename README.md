8c32a8755962ab27b1f820abf15a30af380638f7
amyzliao <amzhliao@gmail.com>
2023-09-01 17:18:48 -0500

    output modifications

---
+++ README.md
@@ -0,0 +1,29 @@
+# cover parser
+For bangyen's very cool research project
+
+# dependencies
+- python virtual environment recommended
+- requests `python -m pip install requests`
+- beautiful soup `python -m pip install beautifulsoup4`
+
+# setup and usage
+if using virtual env:
+```
+cd *into root directory*
+Scripts/activate
+```
+run the parser:
+```
+cd *into root directory*
+py parseCoverHTML.py > output.html
+```
+
+# output
+`output.html` contains output of running `parseCoverHTML.py`.
+This file contains 3 things (in this order):
+1. test to row: mapping of tests to row indices
+2. line coverage matrix: rows correspond to tests, columns correspond to line numbers of the program
+    - 0 means that test does not cover that line
+    - 1 means that test does cover that line
+    - *which line number*? see "ranges of lines considered" in #3 below
+3. stats coverage matrix: maps test row indices to stats and info about that test
--- parseCoverHTML.py
+++ parseCoverHTML.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup
-import os
-
-NUM_TESTS = 20
-NUM_LINES = 293
+import os   
 
 def parse_cover_html(path, row):
     file = open(path, "r")
@@ -15,11 +12,16 @@ def parse_cover_html(path, row):
     covering = False
     covered_lines = 0
     total_lines = 0
-    for line in lines:
+    coverRanges = []
+    rangeStart = None
+    for i, line in enumerate(lines):
         if 'START_COVER' in line.text:
             covering = True
-        if 'FINISH_COVER' in line.text:
+            rangeStart = i
+        if 'FINISH_COVER' in line.text and covering:
             covering = False
+            rangeEnd = i
+            coverRanges.append([rangeStart, rangeEnd])
             break # may remove later
         if covering:
             covered = line.find_all('span', class_='covered')
@@ -33,6 +35,7 @@ def parse_cover_html(path, row):
                     covered_lines += 1
 
     rowStats = {
+        'ranges of lines considered [start, stop]': coverRanges,
         'covered lines': covered_lines,
         'total lines': total_lines,
         'percent of lines covered': covered_lines/total_lines
@@ -40,14 +43,42 @@ def parse_cover_html(path, row):
 
     return rowStats
 
+def countTotalLines(path):
+    file = open(path, "r")
+    contents = file.read()
+    soup = BeautifulSoup(contents, 'html.parser')
+
+    result = soup.find('div', class_='file-lines')
+    lines = result.find_all('div', class_='line')
+
+    covering = False
+    total_lines = 0
+    for i, line in enumerate(lines):
+        if 'START_COVER' in line.text:
+            covering = True
+        if 'FINISH_COVER' in line.text and covering:
+            covering = False
+            break # may remove later
+        if covering:
+            covered = line.find_all('span', class_='covered')
+            uncovered = line.find_all('span', class_='uncovered')
+            if len(covered) > 0 or len(uncovered) > 0:
+                # the line has some coverage or uncoverage (it's not irrelevant)
+                total_lines += 1
+    return total_lines
 
 def parse_folder(folderPath):
+    # get all the files inside the folder
+    # also serves as map of index to test number
+    filelist = os.listdir(folderPath)
+    NUM_TESTS = len(filelist)
+    # determine number of lines up for consideration
+    NUM_LINES = countTotalLines(folderPath + filelist[0])
+    # rows are tests, columns are lines
     # 0 indicates uncovered; 1 indicates covered
     lineCoverageMatrix = [[0 for i in range(NUM_LINES)] for i in range(NUM_TESTS)]
     # maps test number to index
     testToRow = {}
-    # serves as map of index to test number
-    filelist = os.listdir(folderPath)
     # maps test index to stats
     statsMatrix = [None for i in range(NUM_TESTS)]
 
@@ -56,13 +87,13 @@ def parse_folder(folderPath):
         statsMatrix[i] = parse_cover_html(folderPath + file, lineCoverageMatrix[i])
 
     print('test to row')
-    print(testToRow)
+    for key,value in testToRow.items():
+        print('{ ', key, ': ', value, ' }')
     print('line coverage matrix')
-    for row in lineCoverageMatrix:
-        print(row)
+    for i, row in enumerate(lineCoverageMatrix):
+        print(i, row)
     print('stats matrix')
-    for row in statsMatrix:
-        print(row)
+    for i, row in enumerate(statsMatrix):
+        print(i, row)
     
-
 parse_folder('../zombie-coverage/coverage-new/files-cover/')
